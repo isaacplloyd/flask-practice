b04103d2c2f73c4672052c725785fa137ea42ed9
isaacplloyd <31959039+isaacplloyd@users.noreply.github.com>
2018-02-20 18:32:30 -0500

    added poloniex module and get-ltc command

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import poloniex
+
+polo = poloniex.Poloniex()
 
 #practice - Hello world program at web server home directory
 app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello world!"
+
+@app.route('/get-ltc')
+def get_ltc():
+    price = polo('returnTicker')['USDT_LTC']['last']
+	msg = "USDT_LTC last: " + price
+	return msg
 
 if __name__ == "__main__":
 	app.run(host='127.0.0.1')
